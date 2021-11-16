126fb1ee58a16ebf60b951afb5b15236905a6af3
Karthik <kgnaircochin02@gmail.com>
2021-11-17 01:16:03 +0530

    changed the design of confirmation page

--- confirmation.php
+++ confirmation.php
@@ -18,6 +18,7 @@
         integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <link rel="icon" type="image/png" href="img/logo.png">
 </head>
+
 <body style="background-color: #04192e">
     <header></header>
    
@@ -28,46 +29,80 @@
         $result1= mysqli_query($link1, $sql1); 
         $row1 = $result1->fetch_assoc();
         $price=$row1['price'];
-        // echo '<div><h1>&nbsp</h1></div>';
-        // echo '<div>&nbsp</div>';
+        
         echo '<div class="movie-box" style="width:300px;height: 350px;">';
-
         echo '<img src="'. $row1['movieImg'] .'" alt=" ">';
         echo '</div>';
+
         $i=0;
         $link = mysqli_connect("localhost", "root", "", "cinema_db");
         $sql = "SELECT * FROM bookingtable where movieName='$movieName' AND bookingTheatre='$bookingTheatre' AND bookingDate='$bookingDate' AND bookingTime='$bookingTime'";
         $result= mysqli_query($link, $sql);  
 
-        echo '<div style="color:white"><h1><b>Customer Email: </b></h1>';
-            echo $EMAIL;
-            echo '<br>';
-            echo '<h1><b>Movie Name: </b></h1>';
+        echo '<div class="confirmation-panel">';
+            echo '<div class="title">';
+            // echo '<h1><b>Movie Name: </b></h1>';
             echo $movieName;
-            echo '<br>';
-            echo '<h1><b>Booking Theatre: </b></h1>';
-            echo $bookingTheatre;
-            echo '<br>';
-            echo '<h1><b>Booking Date: </b></h1>';
-            echo $bookingDate;
-            echo '<br>';
-            echo '<h1><b>Booking Time: </b></h1>';
-            echo $bookingTime;
-            echo '<br>';
-            echo '<h1><b>Seat Number: </b></h1>';
+            echo '</div>';
+            echo '<div class="confirmation-details">';
+            echo '<table>';
+                echo '<tr>';
+                    echo '<td>';
+                    echo 'CUSTOMER EMAIL';
+                    echo '</td>';
+                    echo '<td>';
+                    echo $EMAIL;
+                    echo '</td>';
+                echo '</tr>';
+                echo '<tr>';
+                    echo '<td>';
+                    echo 'BOOKING THEATRE';
+                    echo '</td>';
+                    echo '<td>';
+                    echo $bookingTheatre;
+                    echo '</td>';
+                echo '</tr>';
+                echo '<tr>';
+                    echo '<td>';
+                    echo 'BOOKING DATE';
+                    echo '</td>';
+                    echo '<td>';
+                    echo $bookingDate;
+                    echo '</td>';
+                echo '</tr>';
+                echo '<tr>';
+                    echo '<td>';
+                    echo 'BOOKING TIME';
+                    echo '</td>';
+                    echo '<td>';
+                    echo $bookingTime;
+                    echo '</td>';
+                echo '</tr>';
+                echo '<tr>';
+                    echo '<td>';
+                    echo 'SEAT NUMBER';
+                    echo '</td>';
+                    echo '<td>';
         while($row = $result->fetch_assoc()){
             $i=$i+1;
-            echo $row['Seat_no'];
-            echo '<br>';
-
+                    
+                    echo $row['Seat_no'];
+                    echo ', ';
+                   
         }
-
-        echo '<br>';
-         echo '<h1><b>Total Price: </b></h1>';
-            echo '₹';
-            echo $i*$price;
-            echo '<br>';
-
+                    echo '</td>';
+                echo '</tr>';
+                echo '<tr>';
+                    echo '<td>';
+                    echo 'TOTAL PRICE';
+                    echo '</td>';
+                    echo '<td style="color:white"><b>';
+                    echo '₹';
+                    echo $i*$price;
+                    echo '</b></td>';
+                echo '</tr>';
+        echo '</table>';
+        echo '</div>';
         echo '<form id="3" action="login.html" name="myform"  method="POST">';
          echo'<br>';
          echo '<button type="submit" value="confirm3" name="confirm3" class="seat-selection-button">LOG OUT</button>';
@@ -76,6 +111,7 @@
         ?>
     
     </div>
+    <br><br>
     
     <footer></footer>
 
