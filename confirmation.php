353322bfb3d8ed241b91f28cb33d799ce7784962
KaushalBarhate <79403580+KaushalBarhate@users.noreply.github.com>
2021-11-16 12:09:37 +0530

    confirmation page

--- confirmation.php
+++ confirmation.php
@@ -1,8 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-<!-- <?php -->
-//         $id=$_GET['type'];
-// ?>
+<?php
+ $EMAIL=$_GET['Email'];
+ $movieName=$_GET['moviename'];
+ $bookingTheatre=$_GET['bookingTheatre'];
+ $bookingType=$_GET['bookingType'];
+ $bookingDate=$_GET['bookingDate'];
+ $bookingTime=$_GET['bookingTime'];
+?>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -17,7 +22,44 @@
    
     <div>
         <?php
-            echo $id;
+        $link1 = mysqli_connect("localhost", "root", "", "cinema_db");
+        $sql1 = "SELECT movieImg FROM movietable WHERE movieTitle='$movieName'";
+        $result1= mysqli_query($link1, $sql1); 
+        $row1 = $result1->fetch_assoc();
+        echo '<img src="'. $row1['movieImg'] .'" alt=" ">';
+
+
+
+        $link = mysqli_connect("localhost", "root", "", "cinema_db");
+        $sql = "SELECT * FROM bookingtable where movieName='$movieName' AND bookingTheatre='$bookingTheatre' AND bookingDate='$bookingDate' AND bookingTime='$bookingTime'";
+        $result= mysqli_query($link, $sql);  
+        echo '<h1><b>Customer Email: </b></h1>';
+            echo $EMAIL;
+            echo '<br>';
+            echo '<h1><b>Movie Name: </b></h1>';
+            echo $movieName;
+            echo '<br>';
+            echo '<h1><b>Booking Theatre: </b></h1>';
+            echo $bookingTheatre;
+            echo '<br>';
+            echo '<h1><b>Booking Date: </b></h1>';
+            echo $bookingDate;
+            echo '<br>';
+            echo '<h1><b>Booking Time: </b></h1>';
+            echo $bookingTime;
+            echo '<br>';
+            echo '<h1><b>Seat Number: </b></h1>';
+        while($row = $result->fetch_assoc()){
+            echo $row['Seat_no'];
+            echo '<br>';
+
+        }
+
+        echo '<form id="3" action="login.html" name="myform"  method="POST">';
+         echo'<br>';
+         echo '<button type="submit" value="confirm3" name="confirm3" class="seat-selection-button">LOG OUT</button>';
+         echo '</form>';
+
         ?>
     </div>
     
